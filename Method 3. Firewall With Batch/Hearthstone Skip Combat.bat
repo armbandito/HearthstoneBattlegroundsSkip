18dc7bb0d417cda477e4da9e89bc6e5cc65d4ff5
BuildTools <unconfigured@null.spigotmc.org>
2025-07-10 11:14:01 -0600

    .

---
+++ Method 3. Firewall With Batch/Hearthstone Skip Combat.bat
@@ -0,0 +1,19 @@
+@echo off
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+	:: Create the new rule
+	netsh advfirewall firewall add rule name="Hearthstone Skip Combat" dir=out action=block protocol=TCP remoteport=3724
+
+	:: Check if rule is indeed created
+	netsh advfirewall firewall show rule name="Hearthstone Skip Combat" > NUL 2>&1
+	IF ERRORLEVEL 1 (
+			ECHO Failed to create rule!
+			pause
+			exit
+	)
+	::Wait 3 seconds
+	timeout /T 3
+	ECHO Removing rule
+
+	::Delete Rule
+	netsh advfirewall firewall delete rule name="Hearthstone Skip Combat"
+exit
